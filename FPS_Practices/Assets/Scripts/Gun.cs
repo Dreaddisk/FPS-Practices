f21e6f5965c70f90e8d0f4643f246428d02ef48c
Dreaddisk <joey_manuel@outlook.com>
2018-09-27 15:22:32 -0400

    5. Fixing issues
    Fixed the Raycast problem. Created a damage counter to the box.

--- FPS_Practices/Assets/Scripts/Gun.cs
+++ FPS_Practices/Assets/Scripts/Gun.cs
@@ -7,15 +7,23 @@ public class Gun : MonoBehaviour
 	#region variables
 	public float damage = 10f;
 	public float range = 100f;
+	public float fireRate = 15f;
+	public float impactForce = 30f;
+
 
 	public Camera fpsCam;
+	public ParticleSystem muzzleFlash;
+
+	private Target _target;
+	private float nextTimeToFire = 0f;
 	#endregion
 
 	#region UnityFunctions
 	void Update()
 	{
-		if(Input.GetMouseButtonDown(0))
+		if(Input.GetMouseButtonDown(0) && Time.time >= nextTimeToFire)
 		{
+			nextTimeToFire = Time.time + 1f / fireRate;
 			Shoot();
 		}
 	}
@@ -24,10 +32,20 @@ public class Gun : MonoBehaviour
 
 	void Shoot()
 	{
+		muzzleFlash.Play();
 		RaycastHit hit;
+		Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
 		if  (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
-		{
+		{			
 			Debug.Log(hit.transform.name);
+			Debug.DrawRay(transform.position, forward, Color.red);
+
+			 _target = hit.transform.GetComponent<Target>();
+
+			if(_target != null)
+			{
+				_target.takeDamage(damage);
+			}
 		}
 	}
 }
---
+++ FPS_Practices/Assets/Scripts/Target.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour 
+{
+	#region Variables
+	public float health = 50f;
+
+	public void takeDamage(float amount)
+	{
+		health -= amount;
+		if(health <= 0f)
+		{
+			Die();
+		}
+	}
+
+	void Die()
+	{
+		Destroy(gameObject);
+	}
+	#endregion
+}// Main Class
